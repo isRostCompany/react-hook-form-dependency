6ef4a3ba44855699da2cf403dbb5c49ba1a093ac
Illia <illya_s@ukr.net>
2022-04-30 18:15:32 +0300

    minimize amount of code

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
+debugger;
 import { useFormContext } from 'react-hook-form';
-
-console.log('from react-hook-form-dependency', useFormContext);
 
 // @ts-ignore
 window.fromReactHookFormDependency = useFormContext;
